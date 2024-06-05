b8b8be9b03816bc770358c6e6fd11be9df2f0acf
Sasha <srush.research@gmail.com>
2024-06-05 14:58:01 -0400

    mess around

--- zoning/utils.py
+++ zoning/utils.py
@@ -8,59 +8,10 @@ from typing import Any, Iterable, TypeVar
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 from openai import APIConnectionError, APIError, OpenAI, RateLimitError, Timeout
-from pydantic import BaseModel
 from tenacity import retry, retry_if_exception_type, wait_random_exponential
 from typer import Typer
 
 T = TypeVar("T")
-
-
-class District(BaseModel):
-    full_name: str
-    short_name: str
-
-
-class PageSearchOutput(BaseModel):
-    text: str
-    page_number: int
-    highlight: list[str]
-    score: float
-    query: str
-
-
-class ExtractionOutput(BaseModel):
-    extracted_text: list[str]
-    rationale: str
-    answer: str | None
-
-    def __str__(self):
-        return f"ExtractionOutput(extracted_text={self.extracted_text}, rationale={self.rationale}, answer={self.answer})"
-
-
-class ExtractionOutput2(BaseModel):
-    district_explanation: str
-    district: str
-    term_explanation: str
-    term: str
-    explanation: str
-    answer: str | None
-
-    def __str__(self):
-        return f"ExtractionOutput(extracted_text={self.extracted_text}, rationale={self.rationale}, answer={self.answer})"
-
-
-class LookupOutput(BaseModel):
-    output: ExtractionOutput | ExtractionOutput2 | None
-    search_pages: list[PageSearchOutput]
-    search_pages_expanded: list[int]
-    """The set of pages, in descending order or relevance, used to produce the
-    result."""
-
-    def __str__(self):
-        return f"LookupOutput(output={self.output}, search_pages=[...], search_pages_expanded={self.search_pages_expanded})"
-
-    def to_dict(self):
-        return json.loads(self.model_dump_json())
 
 
 class EvaluationMetrics(BaseModel):
