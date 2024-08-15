fe449de27861ed77cb51019478e27d1361e7caa4
nanjiangwill <willjiang2018@gmail.com>
2024-08-15 10:51:38 -0400

    update streamlit for deployment

--- viz/viz_user_mode.py
+++ viz/viz_user_mode.py
@@ -392,8 +392,6 @@ else:
     for show_page in showed_pages:
         page = doc.load_page(show_page - 1)
         page_rect = page.rect
-        # for zoom in
-        page_rect += (50, 50, -50, -50)
         page_info = [i for i in format_ocr_result.pages if i["page"] == str(show_page)]
         assert len(page_info) == 1
         page_info = page_info[0]
@@ -471,7 +469,7 @@ else:
         mat = fitz.Matrix(zoom, zoom)
         
         # Render the page to a PIL Image
-        pix = page.get_pixmap(matrix=mat, clip=page_rect)
+        pix = page.get_pixmap(matrix=mat)
         # pix = page.get_pixmap()
         img_bytes = pix.pil_tobytes(format="PNG")
         edited_pages.append(img_bytes)
