150c7cd7e4f4d3036602c8017d74726dcd523a9b
nanjiangwill <willjiang2018@gmail.com>
2024-11-06 14:20:13 -0500

    update viz

--- viz/viz_user_mode_batch_production.py
+++ viz/viz_user_mode_batch_production.py
@@ -1176,32 +1176,35 @@ with st.form("my_form", border=False):
             del st.session_state[f"selected_{i}"]
             st.session_state[f"selected_{i}"] = True
     for i in range(batch_number):
-        with cols[i]:
-            st.checkbox("This is *correct*", key=f"selected_{i}", value=True)
-            st.html(card_html_list[i])
-            st.write("""<div class='PortMarker'/>""", unsafe_allow_html=True)
+        cols[i].checkbox("This is *correct*", key=f"selected_{i}", value=True)
+        cols[i].markdown(card_html_list[i], unsafe_allow_html=True)
 
-    st.write('''<style>
-        [data-testid="stHorizontalBlock"]:has(div.PortMarker) [data-testid="column"] {
-            min-width: 320px !important;
-            width: 320px !important;
-            padding: 0 10px;
-        }
-        [data-testid="stHorizontalBlock"] {
-            overflow-x: auto;
-            white-space: nowrap;
-            display: flex;
-            flex-wrap: nowrap;
-            gap: 1rem;
-            padding: 1rem;
-        }
-        [data-testid="column"]>div>div>div>div>div {
-            overflow-y: auto;
-        }
-        section.main>div {
-            padding-bottom: 1rem;
-        }
-    </style>''', unsafe_allow_html=True)
+    css = """
+<style>
+    section.main>div {
+        padding-bottom: 1rem;
+    }
+    [data-testid="column"] {
+        min-width: 320px !important;
+        width: 320px !important;
+        padding: 0 10px;
+    }
+    [data-testid="column"]>div>div>div>div>div {
+        overflow-y: auto;
+        # height: 70vh;
+    }
+    div[data-testid="stHorizontalBlock"] {
+        overflow-x: auto;
+        white-space: nowrap;
+        display: flex;
+        flex-wrap: nowrap;
+        gap: 1rem;
+        padding: 1rem;
+    }
+</style>
+"""
+
+    st.markdown(css, unsafe_allow_html=True)
 
     st.form_submit_button("Submit batch", on_click=button_callback("correct"))
 
